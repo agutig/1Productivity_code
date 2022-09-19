09e65613a4bd8b1bb6cf140dfbd48d4848845713
agutig <hodeadmusic@gmail.com>
2022-09-19 21:10:18 +0200

    drag and drop for lists

--- src/components/taskManagerManager.jsx
+++ src/components/taskManagerManager.jsx
@@ -3,6 +3,7 @@ import TaskManager from './taskManager';
 import {useState ,useEffect} from 'react'
 import {v4 as uuidv4} from  'uuid'
 import '../styles/taskManagerManager.css'
+import { DragDropContext , Droppable, Draggable} from 'react-beautiful-dnd';
 
 function TaskManagerManager(props){
 
@@ -28,15 +29,55 @@ function TaskManagerManager(props){
     },[taskManagerList ,  props.save])
 
 
+    const reorder = (list, startIndex ,endIndex) => {
+        const result = [...list];
+        const [removed] = result.splice(startIndex,1);
+        result.splice(endIndex,0,removed)
+        return result;
+    }
+
 
     return(
         <div className='taskManagerManager'>
-            <div className='TMMTitle'>
-                <p className='TMMTitleText'>Lists</p>
-                <button className='TMMTitleButton' onClick={() => addTaskManager()}>+</button>
-            </div>
-            {taskManagerList.map( (taskManager) => <TaskManager list={taskManager} key={taskManager.id} id={taskManager.id} db={taskManagerList} refresh={setTaskManagerList}/>)}
-            <p className='br'><br></br></p>
+            <DragDropContext onDragEnd={(result) => 
+                {
+                    const {source , destination} = result;
+                    if(!destination){
+                        return ;
+                    }
+                    if(
+                        source.index === destination.index && source.droppableId === destination.droppableId
+                    ){
+                        return ;
+                    }
+                    setTaskManagerList( prevTask => reorder(prevTask ,source.index,destination.index))
+                }
+            }>
+                <div className='TMMTitle'>
+                    <p className='TMMTitleText'>Lists</p>
+                    <button className='TMMTitleButton' onClick={() => addTaskManager()}>+</button>
+                </div>
+                <Droppable droppableId='taskManager'>
+                    { (droppableProvided) => (
+                        <div {...droppableProvided.droppableProps} ref={droppableProvided.innerRef}>
+                            {taskManagerList.map( (taskManager ,index) => (
+                                <Draggable key={taskManager.id}  draggableId={taskManager.id} index={index}>
+                                    {(draggableProvided) => (
+                                        <div {...draggableProvided.draggableProps} ref={draggableProvided.innerRef} {...draggableProvided.dragHandleProps}>
+                                            <TaskManager list={taskManager} key={taskManager.id} id={taskManager.id} db={taskManagerList} refresh={setTaskManagerList}/>
+                                        </div>
+                                    )
+                                    }
+                                </Draggable>
+                            ))  
+                            }
+                            <p className='br'><br></br></p>
+                            {droppableProvided.placeholder}
+                        </div>
+                    )
+                    }
+                </Droppable>
+            </DragDropContext>
         </div>
     );
 
